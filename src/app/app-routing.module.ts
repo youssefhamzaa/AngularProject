7cd6786d5c0691b07ee51657d41090afc08023c5
Hsouna <ala.hassouna.tn@gmail.com>
2025-02-07 16:29:53 +0100

    2eme seance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MemberFormComponent } from './member-form/member-form.component';
+import { MemberComponent } from './member/member.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'create',
+    pathMatch:'full',//verification stricte il faut avoir /create ni moins ni plus 
+    component:MemberFormComponent
+  
+  },
+  {path:'',
+    pathMatch:'full',//verification stricte il faut avoir /create ni moins ni plus 
+    component:MemberComponent
+  
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
